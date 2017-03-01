9ef0f1751ecb774e77d1ad3b0393d6c67f9446b5
Denis Makovskyy <makovskyy0983@gmail.com>
2017-03-01 20:10:51 +0200

    Page speed optimization

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,32 +1,30 @@
-$(window).scroll(function() {
-	var yScroll = $(this).scrollTop();
-	var opacity, z;
-	$(".title").css({
-		"transform": "translateY(" + yScroll / 2 + "%)"
-	});
+function init() {
+	$(window).scroll(function() {
+		var yScroll = $(this).scrollTop();
+		var opacity, z;
+		$(".title").css({
+			"transform": "translateY(" + yScroll / 2 + "%)"
+		});
 
-	opacity = Math.min(Math.max(1 - (yScroll - 100) / 150, 0), 1);
-	z = yScroll > 250 ? -10 : 200;
-	$(".links").css({ "opacity": opacity, "z-index": z}); 
-});
-
-
-$(document).ready(function(){
-
-	$(".links ul a").click(function(){
-		var $href = $(this).attr("href");
-		$("body").stop().animate({
-			scrollTop: $($href).offset().top
-		}, 1000);
-		return false;
+		opacity = Math.min(Math.max(1 - (yScroll - 100) / 150, 0), 1);
+		z = yScroll > 250 ? -10 : 200;
+		$(".links").css({ "opacity": opacity, "z-index": z}); 
 	});
 
 
-	for(var i = 1; i <= 27; i++) {
-		var duration = 1 + Math.random() * 3;
-		$("#star" + i).css({
-			"animation" : duration + "s gleam infinite ease"
-		})
-	}
+	$(document).ready(function(){
 
-});
+		$("head").append('<link href="https://fonts.googleapis.com/css?family=Lora:400,700&amp;subset=cyrillic" rel="stylesheet">');
+		$("head").append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">');
+		$("head").append('<link rel="stylesheet" type="text/css" href="portfolio/styles/main.css">');
+
+		$(".links ul a").click(function(){
+			var $href = $(this).attr("href");
+			$("body").stop().animate({
+				scrollTop: $($href).offset().top
+			}, 1000);
+			return false;
+		});
+
+	});
+}
